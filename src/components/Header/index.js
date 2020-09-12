8a31aeed5b8e003d8a703fdee5f4742d50d3d481
Sameer Avhad <sameeravhad119@gmail.com>
2020-09-12 12:51:09 +0530

    brand name set

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './index.css';
+import Logo from "../../assets/react.png";
 
 export const Header = (props) => {
+    const { brandName, logo } = props;
     return (
-        <div>
-
+        <div className={"header"}>
+            <img src={Logo} alt={'company logo'} className={'header_logo'} />
+            <span className={'header_name'}>{brandName}</span>
         </div>
     )
 }
